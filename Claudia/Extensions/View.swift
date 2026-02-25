34560124acf9947465cbca66b1314a8388786881
Sherlock Luk <sherlockluk@outlook.com>
2026-02-25 14:15:36 +0000

    Add always transparent scrollbars

--- Claudia/Extensions/View.swift
+++ Claudia/Extensions/View.swift
@@ -6,6 +6,26 @@
 //
 
 import SwiftUI
+import SwiftUIIntrospect
+
+// Taken from https://github.com/lockieluke/cider-swiftui/blob/e87ba9c48b90aa172728d5746ad06e62b6d48539/Cider/Views/ViewModifiers/TransparentScrollbarsModifier.swift
+private struct TransparentScrollbarsModifier: ViewModifier {
+    
+    var enabled: Bool
+    
+    func body(content: Content) -> some View {
+#if canImport(AppKit)
+        content
+            .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13, .v14)) { scrollView in
+                scrollView.autohidesScrollers = true
+                scrollView.scrollerStyle = .overlay
+            }
+#else
+        return content
+#endif
+    }
+    
+}
 
 extension View {
     
@@ -19,6 +39,10 @@ extension View {
     
     func erasedToAnyView() -> AnyView {
         AnyView(self)
+    }
+    
+    func transparentScrollbars(_ enabled: Bool = true) -> some View {
+        self.modifier(TransparentScrollbarsModifier(enabled: enabled))
     }
     
 }
